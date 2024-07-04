2d632edc508c07e4ba51c874785c2a925d172d49
MicrQ <mail.micrq@proton.me>
2024-07-04 22:37:00 +0300

    a function that concatenats strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" more type annotation practice """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ a function that concatenats given two strings """
+    return str1 + str2
