17d879cec12387049ea3c6bdce0e5595f54b7783
MicrQ <mail.micrq@proton.me>
2024-07-04 22:59:47 +0300

    a function that returns sum of floats of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" complex types """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ a funtion that returns the sum of floats of a list """
+    return sum(input_list)
